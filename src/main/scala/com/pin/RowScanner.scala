5ef1eb82d2c23f04849b77669cd9fed7decb7de0
Pin <Alexei.Antipin@googlemail.com>
2020-01-26 16:07:33 +0000

    Refactoring - encapsulating logic within event case classes

--- src/main/scala/com/pin/RowScanner.scala
+++ src/main/scala/com/pin/RowScanner.scala
@@ -13,15 +13,90 @@ trait WithContent extends ParserEvent {
   val content: String
 }
 
-case class CellParced(input: String, position: Int, content: String) extends ParserEvent with WithContent
+trait IntermediateParserEvent extends ParserEvent with WithContent {
+  def nextEvent(implicit delimiters: Delimiters): ParserEvent
+}
 
-case class CellIncomplete(input: String, position: Int, content: String) extends ParserEvent with WithContent
+case class CellParced(input: String, position: Int, content: String) extends IntermediateParserEvent {
+  override def nextEvent(implicit delimiters: Delimiters): ParserEvent = {
+    if (position > input.length)
+      RowComplete(input, position)
+    else
+      CellIncomplete(input, position, "")
+  }
+}
 
-case class Escaped(input: String, position: Int, content: String) extends ParserEvent with WithContent
+case class CellIncomplete(input: String, position: Int, content: String) extends IntermediateParserEvent {
 
-case class Quoted(input: String, position: Int, content: String) extends ParserEvent with WithContent
+  override def nextEvent(implicit delimiters: Delimiters): ParserEvent = {
+    val newPos = input.indexWhere(c => c == delimiters.quote || c == delimiters.cellSeparator || c == delimiters.escape, position)
+    if (newPos == -1) {
+      // end of line
+      val cellValue = input.substring(position)
+      CellParced(input, input.length + 1, content ++ cellValue)
+    } else {
+      val delimiter = input(newPos)
 
-case class QuotedEscaped(input: String, position: Int, content: String) extends ParserEvent with WithContent
+      delimiter match {
+        case delimiters.quote =>
+          if (newPos != position) {
+            val newContent = input.substring(position, newPos)
+            CellIncomplete(input, newPos, content ++ newContent)
+          } else
+            Quoted(input, position, content)
+        case delimiters.cellSeparator =>
+          val newContent = input.substring(position, newPos)
+          CellParced(input, newPos + 1, content ++ newContent)
+        case delimiters.escape =>
+          val newContent = input.substring(position, newPos)
+          Escaped(input, newPos + 1, content ++ newContent)
+      }
+    }
+  }
+}
+
+case class Escaped(input: String, position: Int, content: String) extends IntermediateParserEvent {
+  override def nextEvent(implicit delimiters: Delimiters): ParserEvent = {
+    if (position < input.length) {
+      val newContent = input(position)
+      CellIncomplete(input, position + 1, content + newContent)
+    } else {
+      // Escaped line end, undefined behavior. Let's simply add escape character to the value of the cell
+      CellParced(input, position + 1, content + delimiters.escape)
+    }
+  }
+}
+
+case class Quoted(input: String, position: Int, content: String) extends IntermediateParserEvent {
+  override def nextEvent(implicit delimiters: Delimiters): ParserEvent = {
+    val delimiterPosition = input.indexWhere(c => c == delimiters.quote || c == delimiters.escape, position + 1)
+
+    if (delimiterPosition == -1) {
+      QuotedIncomplete(input, position, content + input.substring(position))
+    } else {
+      input(delimiterPosition) match {
+        case delimiters.quote =>
+          // content includes quote symbol
+          CellIncomplete(input, delimiterPosition + 1, content + input.substring(position, delimiterPosition + 1))
+        case delimiters.escape =>
+          // content does not include escape symbol
+          QuotedEscaped(input, delimiterPosition + 1, content + input.substring(position, delimiterPosition))
+      }
+    }
+  }
+}
+
+case class QuotedEscaped(input: String, position: Int, content: String) extends IntermediateParserEvent {
+  override def nextEvent(implicit delimiters: Delimiters): ParserEvent = {
+    if (position < input.length) {
+      val newContent = input(position)
+      Quoted(input, position + 1, content + newContent)
+    } else {
+      // Escaped line end, undefined behavior. Let's simply add escape character to the value of the cell
+      CellParced(input, position + 1, content + delimiters.escape)
+    }
+  }
+}
 
 case class QuotedIncomplete(input: String, position: Int, content: String) extends ParserEvent with WithContent
 
@@ -38,75 +113,9 @@ object RowScanner {
 
   def startLine(input: String): ParserEvent = CellIncomplete(input, 0, "")
 
-  def scan(state: ParserEvent)(implicit delimiters: Delimiters): ParserEvent = {
-    state match {
-      case CellIncomplete(input, pos, content) =>
-        val newPos = input.indexWhere(c => c == delimiters.quote || c == delimiters.cellSeparator || c == delimiters.escape, pos)
-
-        if (newPos == -1) {
-          // end of line
-          val cellValue = input.substring(pos)
-          CellParced(input, input.length + 1, content ++ cellValue)
-        } else {
-          val delimiter = input(newPos)
-
-          delimiter match {
-            case delimiters.quote =>
-              if (newPos != pos) {
-                val newContent = input.substring(pos, newPos)
-                CellIncomplete(input, newPos, content ++ newContent)
-              } else
-                Quoted(input, pos, content)
-            case delimiters.cellSeparator =>
-              val newContent = input.substring(pos, newPos)
-              CellParced(input, newPos + 1, content ++ newContent)
-            case delimiters.escape =>
-              val newContent = input.substring(pos, newPos)
-              Escaped(input, newPos + 1, content ++ newContent)
-          }
-        }
-
-      case Escaped(input, pos, content) =>
-        if (pos < input.length) {
-          val newContent = input(pos)
-          CellIncomplete(input, pos + 1, content + newContent)
-        } else {
-          // Escaped line end, undefined behavior. Let's simply add escape character to the value of the cell
-          CellParced(input, pos + 1, content + delimiters.escape)
-        }
-
-
-      case Quoted(input, pos, content) =>
-        val delimiterPosition = input.indexWhere(c => c == delimiters.quote || c == delimiters.escape, pos + 1)
-
-        if (delimiterPosition == -1) {
-          QuotedIncomplete(input, pos, content + input.substring(pos))
-        } else {
-          input(delimiterPosition) match {
-            case delimiters.quote =>
-              // content includes quote symbol
-              CellIncomplete(input, delimiterPosition + 1, content + input.substring(pos, delimiterPosition + 1))
-            case delimiters.escape =>
-              // content does not include escape symbol
-              QuotedEscaped(input, delimiterPosition + 1, content  + input.substring(pos, delimiterPosition))
-          }
-        }
-
-      case QuotedEscaped(input, pos, content) =>
-        if (pos < input.length) {
-          val newContent = input(pos)
-          Quoted(input, pos + 1, content + newContent)
-        } else {
-          // Escaped line end, undefined behavior. Let's simply add escape character to the value of the cell
-          CellParced(input, pos + 1, content + delimiters.escape)
-        }
-
-      case CellParced(input, position, _) =>
-        if (position > input.length)
-          RowComplete(input, position)
-        else
-          CellIncomplete(input, position, "")
-    }
+  // Partial, not defined for not intermediate events
+  def scan(state: ParserEvent)(implicit delimiters: Delimiters): ParserEvent = state match {
+    case ipe: IntermediateParserEvent => ipe.nextEvent
   }
 
 
